56a47cf5ebfaad44548b5ea01527fbc595acc0b0
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-10-26 10:53:33 +0300

    task 3

--- build_index.py
+++ build_index.py
@@ -5,6 +5,15 @@ from langchain_community.document_loaders import TextLoader
 import os
 import glob
 
+DEVICE = "cpu"
+MODEL_NAME = "BAAI/bge-base-en-v1.5"
+#MODEL_NAME = "cointegrated/rubert-tiny2"
+#MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+#MODEL_NAME = "sentence-transformers/all-mpnet-base-v2"
+#MODEL_NAME = "BAAI/bge-m3"
+#MODEL_NAME = "ai-forever/sbert_large_mt_nlu_ru"
+#MODEL_NAME = "DeepPavlov/rubert-base-cased-sentence"
+
 def create_chunks(file_path):
     loader = TextLoader(file_path, encoding='utf-8')
     documents = loader.load()
@@ -18,14 +27,14 @@ def create_chunks(file_path):
             ". ", "! ", "? ",  # предложения
             " ",               # слова
             ""
-        ]
+        ],
+        keep_separator=False
     )
     chunks = text_splitter.split_documents(documents)
     print(f"Создано {len(chunks)} чанков")
     return chunks
 
 def create_vectorstore(chunks):
-    device = "cpu"
     print("Добавление метаданных...")
     for i, doc in enumerate(chunks):
         source = doc.metadata['source']
@@ -34,8 +43,8 @@ def create_vectorstore(chunks):
         doc.metadata['chunk_id'] = f"chunk_{i:04d}"
     print("Создание векторного хранилища...")
     embeddings = HuggingFaceEmbeddings(
-        model_name="BAAI/bge-base-en-v1.5",
-        model_kwargs={"device": device},
+        model_name=MODEL_NAME,
+        model_kwargs={"device": DEVICE},
         encode_kwargs={"normalize_embeddings": True}
     )
     vectorstore = FAISS.from_documents(chunks, embeddings)
--- search_index.py
+++ search_index.py
@@ -3,19 +3,30 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 INDEX_DIR = "faiss.index"
 
-device = "cpu"
+DEVICE = "cpu"
+MODEL_NAME = "BAAI/bge-base-en-v1.5"
+#MODEL_NAME = "cointegrated/rubert-tiny2"
+#MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+#MODEL_NAME = "sentence-transformers/all-mpnet-base-v2"
+#MODEL_NAME = "BAAI/bge-m3"
+#MODEL_NAME = "ai-forever/sbert_large_mt_nlu_ru"
+#MODEL_NAME = "DeepPavlov/rubert-base-cased-sentence"
+
+#QUERY_TEXT = "Саша Скил"
+QUERY_TEXT = "кризис подливиона"
+#QUERY_TEXT = "древний манускрипт"
+
 embeddings = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-base-en-v1.5",
-    model_kwargs={"device": device},
+    model_name=MODEL_NAME,
+    model_kwargs={"device": DEVICE},
     encode_kwargs={"normalize_embeddings": True}
 )
 index = FAISS.load_local(INDEX_DIR, embeddings, allow_dangerous_deserialization=True)
 
-query_text = "Саша Скил"
-print(f"Поиск фразы '{query_text}'")
+print(f"Поиск фразы '{QUERY_TEXT}'")
 
 # ищем 5 документов
-docs = index.similarity_search(query_text, k=5)
+docs = index.similarity_search(QUERY_TEXT, k=5)
 
 for number, doc in enumerate(docs, 1):
     print(f"Результат {number}:")
